83a3a7eaede7ff12c834501b212770fb1f4eff5a
robkerrybjss <rob.kerry@bjss.com>
2023-12-11 15:05:52 +0000

    BAU-12873 - Add tests for PRS

--- src/credential-manager/port-redirection-service.ts
+++ src/credential-manager/port-redirection-service.ts
@@ -1,15 +1,12 @@
 import {PortResponse} from "../model/port-response";
+import axios, {AxiosResponse} from "axios";
 
 export class PortRedirectionService {
 
     async locateService(): Promise<PortResponse> {
         const portNumbers = [43487, 44213, 45031, 46478, 48853];
         const tasks = portNumbers.map((pn) =>
-            fetch(`http://localhost:${pn}/PRS/GetPortNumber`, {
-                method: 'GET',
-                cache: 'no-cache',
-                credentials: 'include',
-            }).then((rsp) => rsp.json())
+            axios.get<PortResponse>(`http://localhost:${pn}/PRS/GetPortNumber`).then((rsp: AxiosResponse) => rsp.data)
         );
         return Promise.any(tasks);
     }
