82283a08d221aebbc4b3b84360c1d7e9362aa295
robkerrybjss <rob.kerry@bjss.com>
2023-12-21 11:43:19 +0000

    BAU-12872 - Tidy example to remove references to JWTs

--- examples/react-example-webapp/src/pages/signingComponent.tsx
+++ examples/react-example-webapp/src/pages/signingComponent.tsx
@@ -3,20 +3,20 @@ import {signPrescription, HubResponse} from "../../../../dist/index";
 
 export default function SigningComponent() {
 
-    const [jwt, setJwt] = useState("");
+    const [json, setJson] = useState("");
     const [result, setResult] = useState("");
     const [error, setError] = useState("");
 
-    function signJwt(jwt: string) {
-        signPrescription(jwt).then((result: HubResponse) => setResult(JSON.stringify(result,null,'\t'))).catch(e => {
+    function signJson() {
+        signPrescription(json).then((result: HubResponse) => setResult(JSON.stringify(result,null,'\t'))).catch(e => {
             console.error(e)
             setError(e.message)
         });
     }
 
     return <>
-        <textarea value={jwt} onChange={inputEvent => setJwt(inputEvent.target.value)}/>
-        <button onClick={() => signJwt(jwt)}>Submit</button>
+        <textarea value={json} onChange={inputEvent => setJson(inputEvent.target.value)}/>
+        <button onClick={() => signJson()}>Submit</button>
         <pre style={{
             maxWidth: "80%",
             overflow: "scroll"
